ac4a404bd9c7ca44c014c5930a58a8b0a1041c2a
asha <ashaagm@gmail.com>
2018-04-21 23:59:46 -0400

    initial files

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import logo from "./ccc.png";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Care! Connect! Conquer!</h2>
+        </div>
+        <p className="App-intro">
+          content here
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
