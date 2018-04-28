929f4fe44d9265e06f13b3d39520128e4d0136ff
asha <ashaagm@gmail.com>
2018-04-28 15:40:49 -0400

    donate page

--- client/src/components/Donate/Donate.js
+++ client/src/components/Donate/Donate.js
@@ -1,19 +1,113 @@
 import React, { Component } from 'react';
 import './Donate.css';
+import { Card, Icon, Image, Grid } from 'semantic-ui-react'
 import Checkout from './Checkout';
+import logo from "../../logo.png";
+import image1 from "./image1.png";
+import image2 from "./image2.jpg";
+import image3 from "./image3.jpg";
 
 class Donate extends Component {
   render() {
     return (
-      <div className="App">
-        <p className="App-intro">
-          <Checkout
-            name={'Donate Now'}
-            description={'Creditcards only'}
-            amount={1}
-          />
-        </p>
-      </div>
+      <div>
+        <div className="div-logo">
+          <img src={logo} alt="logo" className="logo-register"/>
+        </div> 
+      <div className="donate-container">
+      <Grid>
+      <Grid.Row columns={3}>
+        <Grid.Column>
+            <Card className="donate-card">
+              <Image src={image2} />
+              <Card.Content>
+                <Card.Header>
+                  Help cancer research
+                </Card.Header>
+                <Card.Meta>
+                  <span className='date'>
+                    Saving lives through research
+                  </span>
+                </Card.Meta>
+                <Card.Description>
+                   Time is running short, do your bit to find a cure for cancer.
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                 <div className="App">
+                    <p className="App-intro">
+                      <Checkout
+                        name={'Donate Now'}
+                        description={'Creditcards only'}
+                        amount={1}
+                      />
+                    </p>
+                </div>
+              </Card.Content>
+            </Card>
+        </Grid.Column>
+        <Grid.Column>
+           <Card className="donate-card">
+              <Image src={image3} />
+              <Card.Content>
+                <Card.Header>
+                  Help Pediatric cancer research
+                </Card.Header>
+                <Card.Meta>
+                  <span className='date'>
+                    Saving lives through research
+                  </span>
+                </Card.Meta>
+                <Card.Description>
+                   Time is running short, do your bit to find a cure for cancer.
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                 <div className="App">
+                    <p className="App-intro">
+                      <Checkout
+                        name={'Donate Now'}
+                        description={'Creditcards only'}
+                        amount={1}
+                      />
+                    </p>
+                </div>
+              </Card.Content>
+            </Card>
+        </Grid.Column>
+        <Grid.Column>
+            <Card className="donate-card">
+              <Image src={image1} />
+              <Card.Content>
+                <Card.Header>
+                   Help a cancer patient for a ride
+                </Card.Header>
+                <Card.Meta>
+                  <span>
+                     $10 = 1 ride to and from treatment for a cancer patient.
+                  </span>
+                </Card.Meta>
+                <Card.Description>
+                   Cancer patients cite transportation to and from medical treatment as a critical need.
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                 <div className="App">
+                    <p className="App-intro">
+                      <Checkout
+                        name={'Donate Now'}
+                        description={'Creditcards only'}
+                        amount={1}
+                      />
+                    </p>
+                </div>
+              </Card.Content>
+            </Card>
+        </Grid.Column>
+      </Grid.Row>
+     </Grid>
+     </div>  
+     </div>
     );
   }
 }
