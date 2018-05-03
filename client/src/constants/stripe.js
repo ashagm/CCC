17e36f81f2096b0e978b6b0cd378f6e68e258a32
asha <ashaagm@gmail.com>
2018-05-03 13:38:29 -0400

    build with stripe keys

--- client/src/constants/stripe.js
+++ client/src/constants/stripe.js
@@ -1,10 +1,10 @@
 const STRIPE_PUBLISHABLE = process.env.NODE_ENV === 'production'
-  ? 'pk_test_PRODUCTION_KEY'
+  ? 'pk_live_w3oNEYB4yuzepO0BYV9z94pj'
   : 'pk_test_17Ff2qs90t6uPu07PYaCjW5U';
 
 export default STRIPE_PUBLISHABLE;
 
-
+//pk_live_w3oNEYB4yuzepO0BYV9z94pj
 // Publishable key - pk_test_MOweM0nfLzAlz2pMG6l2N2v6
 // pk_test_17Ff2qs90t6uPu07PYaCjW5U
 // 
--- constants/stripe.js
+++ constants/stripe.js
@@ -1,10 +1,11 @@
 const configureStripe = require('stripe');
 
 const STRIPE_SECRET_KEY = process.env.NODE_ENV === 'production'
-    ? 'sk_live_MY_SECRET_KEY'
+    ? 'sk_live_iWvhrhkZLSFhwo7jlGyXmLCY'
     : 'sk_test_AyXhB8zari50LMnHPHS57a23';
 
 //sk_test_AyXhB8zari50LMnHPHS57a23
+//sk_live_iWvhrhkZLSFhwo7jlGyXmLCY
 const stripe = configureStripe(STRIPE_SECRET_KEY);
 
 module.exports = stripe;
